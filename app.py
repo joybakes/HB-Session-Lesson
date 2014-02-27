e56bc48e473d9147d81e8794e84096649032d241
Hackbright Student <info@hackbrightacademy.com>
2014-02-26 18:04:16 -0800

    going home, saving my place.

--- app.py
+++ app.py
@@ -6,15 +6,54 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("user_id"):
+        return "User %s is logged in!"%session['user_id']
+    else:    
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    user_id = model.authenticate(username, password)
+
+    if user_id != None:
+        flash("User authenticated")
+        session['user_id'] = user_id
+    else:
+        flash("Password incorrect, there may be dragons.")
+
+    return redirect(url_for("index"))
+
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))   
+
+@app.route("/user/<username>")
+def view_user(username):
+    logged_in = False
+    user_id = model.get_user_by_name(username)[0]
+    if session.get("user_id"):
+        logged_in = True
+
+    if user_id != None:        
+        wall_posts = model.get_wall_posts(user_id)
+        if not wall_posts:
+            return render_template("wall.html", message="This user has no posts.",
+                                                logged_in=logged_in)
+        else:
+            return render_template("wall.html", posts=wall_posts,
+                                                logged_in=logged_in)
+            print wall_posts
+    else:
+        return render_template("wall.html", message="Not a valid user.")
+
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,53 @@
+import sqlite3
+
+DB = None
+CONN = None
+
 ADMIN_USER="hackbright"
 ADMIN_PASSWORD=5980025637247534551
 
 def authenticate(username, password):
-    return False
+    connect_to_db()
+
+    query = """SELECT id from users WHERE username=? AND password=?
+            """
+    DB.execute(query, (username, hash(password)))
+    user_id = DB.fetchone()
+
+    if user_id != None:
+        return user_id
+
+    else:
+        return None
+
+def get_user_by_name(username):
+    connect_to_db()
+    query = """SELECT id from users WHERE username=?
+            """
+    DB.execute(query, (username,))
+    user_id = DB.fetchone()
+
+    if user_id != None:
+        return user_id
+
+    else:
+        return [None]
+
+
+def get_wall_posts(userid):
+    connect_to_db()
+    query = """SELECT author_id, created_at, content
+               FROM wall_posts
+               WHERE owner_id = ?
+            """
+    result = DB.execute(query, (userid,))
+    return result.fetchall()
+    # if result != None:
+    #     return result.fetchall()
+    # else:
+    #     return None
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
